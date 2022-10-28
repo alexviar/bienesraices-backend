27e9334c5dcd50d179ae15a1d6219cf9999db49d
Alexander <alexanderviar@gmail.com>
2022-10-28 08:35:55 -0400

    Checkpoint

---
+++ tests/Feature/Roles/index/BuscarRolesTest.php
@@ -0,0 +1,91 @@
+<?php
+
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+use Tests\TestCase;
+
+test('el usuario ha iniciado sesión', function () {
+    $response = $this->getJson('/api/roles');
+    $response->assertUnauthorized();
+});
+
+#region Pruebas de autorizacion
+test('usuarios sin permiso no estan autorizados', function () {
+    /** @var TestCase $this */
+    /** @var User $login */
+    $login = User::factory([
+        "estado" => 1
+    ])->create();
+    /** @var Role $rol */
+    $rol = Role::factory()->create();
+    $login->assignRole($rol);
+    $response = $this->actingAs($login)->getJson('/api/roles');
+    $response->assertForbidden();
+});
+
+test('usuarios autorizados', function ($dataset) {
+    /** @var TestCase $this */
+    $login = $dataset["login"];
+    $response = $this->actingAs($login)->getJson('/api/roles');
+    expect($response->getStatusCode())->not->toBe(403);
+})->with([
+    "Acceso directo" => function(){
+        /** @var User $login */
+        $login = User::factory([
+            "estado" => 1
+        ])->create();
+        /** @var Role $rol */
+        $rol = Role::factory()->create();
+        $rol->givePermissionTo("Ver roles");
+        $login->assignRole($rol);
+        return [
+            "login" => $login
+        ];
+    },
+    "Acceso indirecto" => function(){
+        /** @var User $login */
+        $login = User::factory([
+            "estado" => 1
+        ])->create();
+        /** @var Role $rol */
+        $rol = Role::factory()->create();
+        $permission = Permission::factory()->create();
+        $permission->givePermissionTo("Ver roles");
+        $rol->givePermissionTo($permission);
+        $login->assignRole($rol);
+        return [
+            "login" => $login
+        ];
+    }
+]);
+#endregion
+
+test('Paginación', function () {
+    /** @var TestCase $this */
+    /** @var User $login */
+    $login = User::factory()->create();
+    $login->assignRole("Super usuarios");
+    Role::factory(11)->create();
+    $response = $this->actingAs($login)->getJson("/api/roles?".http_build_query([
+        "page" => [ "current" => 1, "size" => 10],
+    ]));
+    $response->assertOk();
+    $response->assertJsonCount(10, "records");
+    $response->assertJsonStructure([
+        "meta" => [
+            "total_records"
+        ],
+        "records" => [
+            "*" => [
+                "id",
+                "username",
+                "email",
+                "estado",
+                "estado_text"
+            ]
+        ]
+    ]);
+    $this->assertTrue($response->json("meta.total_records") == 12);
+    $this->assertTrue(count($response->json("records")) == 10);
+});
